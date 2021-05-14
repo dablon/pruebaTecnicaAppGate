7381c18912cd0ada968207a21f689618c2a5643e
Nicolas Alcaraz <nicolasalca@hotmail.com>
2021-05-13 23:44:36 -0500

    remove file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 echo"Starting gunicorn..."
 gunicorn -w 5 -b 127.0.0.1:9000 main:app --daemon
-#gunicorn --chdir app app:app -w 2 --threads 2 -b 0.0.0.0:80
-# gunicorn --chdir app main:app -w 2 --threads 2 -b 0.0.0.0:8000
 
 sleep 3
 echo"Starting haproxy..."
 # haproxy
-# cat /etc/haproxy/haproxy.cfg
 haproxy -f "/etc/haproxy/haproxy.cfg" 
